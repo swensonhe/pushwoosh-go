98f6373ca9f578fe5ce34d7ee05e8e954a9eaf60
Nick Mrozowski <nickmro@gmail.com>
2018-03-29 17:37:37 -0700

    Add Android capabilities

--- notification.go
+++ notification.go
@@ -4,9 +4,11 @@ type Notification struct {
 	SendDate string `json:"send_date"`
 	Content string `json:"content,omitempty"`
 	Devices []string `json:"devices"`
-	IosRootParams `json:"ios_root_params"`
+	IosRootParams IosRootParams `json:"ios_root_params"`
 	IosSound string `json:"ios_sound,omitempty"`
 	IosSilent SilentSetting `json:"ios_silent,omitempty"`
+	AndroidRootParams AndroidRootParams `json:"android_root_params,omitempty"`
+	AndroidSilent SilentSetting `json:"android_slient",omitempty`
 	Platforms []int `json:"platforms"`
 }
 
@@ -14,6 +16,8 @@ type IosRootParams struct {
 	Aps Aps `json:"aps,omitempty"`
 	Data interface{} `json:"data,omitempty"`
 }
+
+type AndroidRootParams map[string]interface{}
 
 type Aps struct {
 	ContentAvailable ContentAvailability `json:"content-available,omitempty"`
