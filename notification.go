234d6e81582b1d20ad68e66b8fa5f510407e940b
Nikita Kolosov <nkolosov@pushwoosh.com>
2019-03-05 13:27:03 +0700

    Fixed android_silent request parameter name

--- notification.go
+++ notification.go
@@ -1,21 +1,21 @@
 package pushwoosh
 
 type Notification struct {
-	SendDate string `json:"send_date"`
-	Content string `json:"content,omitempty"`
-	Devices []string `json:"devices,omitempty"`
-	Users []string `json:"users,omitempty"`
-	IosCategoryId *string `json:"ios_category_id,omitempty"`
-	IosRootParams IosRootParams `json:"ios_root_params"`
-	IosSound string `json:"ios_sound,omitempty"`
-	IosSilent SilentSetting `json:"ios_silent,omitempty"`
+	SendDate          string            `json:"send_date"`
+	Content           string            `json:"content,omitempty"`
+	Devices           []string          `json:"devices,omitempty"`
+	Users             []string          `json:"users,omitempty"`
+	IosCategoryId     *string           `json:"ios_category_id,omitempty"`
+	IosRootParams     IosRootParams     `json:"ios_root_params"`
+	IosSound          string            `json:"ios_sound,omitempty"`
+	IosSilent         SilentSetting     `json:"ios_silent,omitempty"`
 	AndroidRootParams AndroidRootParams `json:"android_root_params,omitempty"`
-	AndroidSilent SilentSetting `json:"android_slient",omitempty`
-	Platforms []int `json:"platforms"`
+	AndroidSilent     SilentSetting     `json:"android_silent,omitempty"`
+	Platforms         []int             `json:"platforms"`
 }
 
 type IosRootParams struct {
-	Aps Aps `json:"aps,omitempty"`
+	Aps  Aps         `json:"aps,omitempty"`
 	Data interface{} `json:"data,omitempty"`
 }
 
